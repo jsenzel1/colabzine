b639986c205d5b46b775bc69b594ae3e16b40c88
Jonah Senzel <jsenzel1@gmail.com>
2021-02-02 17:26:23 +0200

    sorry I'm lazing and autocomitting with a bash alias

--- blankScript.js
+++ blankScript.js
@@ -171,7 +171,7 @@ strangersZine4.initialize();
 const strangersZine5 = new Zine("strangers", "Strangers At Home", "Jonah");
 strangersZine.initialize();
 
-const strangersZine2 = new Zine("strangers", "Strangers At Home", "Jonah");
+const strangersZine5 = new Zine("strangers", "Strangers At Home", "Jonah");
 strangersZine5.initialize();
 
 // strangersZine.rightButton.onclick = strangersZine.onRight;
